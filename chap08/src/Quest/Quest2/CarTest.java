d04c23a50660cd7089c7abf45443958d57681c23
JadeMin <36400787+JadeMin@users.noreply.github.com>
2024-06-17 13:10:55 +0900

    upload chapter `chap08`

--- chap08/src/Quest/Quest2/CarTest.java
+++ chap08/src/Quest/Quest2/CarTest.java
@@ -9,8 +9,10 @@ public class CarTest {
 		Car myCar = new Car("그랜저", "06-07-2017", "홍길동");
 		Car yourCar = new Car("그랜저", "06-07-2017", "홍길동");
 
+
 		if (myCar.equals(yourCar))
 			System.out.println(MessageFormat.format("자동차 모델이 둘 다 [{0}]로 동일하다.", myCar.model));
+
 		System.out.println(MessageFormat.format("날짜: {0}, 자동차 모델=[{1}], 운전자({2})", myCar.date, myCar.model, myCar.rider));
 	}
 }
--- chap08/src/Quest/Quest3/CarTest.java
+++ chap08/src/Quest/Quest3/CarTest.java
@@ -8,10 +8,17 @@ public class CarTest {
 		Car myCar = new Car("그랜저", "06-07-2017", "홍길동");
 		Car yourCar = new Car("그랜저", "06-07-2017", "홍길동");
 
+
 		if (myCar.equals(yourCar))
 			System.out.println(MessageFormat.format("자동차 모델이 둘 다 [{0}]로 동일하다.", myCar.model));
 
-		StringTokenizer st = new StringTokenizer(MessageFormat.format("날짜: {0} 자동차 모델 {1} 운전자 {2}", myCar.date, myCar.model, myCar.rider), " ");
+		StringTokenizer st = new StringTokenizer(
+				MessageFormat.format(
+						"날짜: {0} 자동차 모델 {1} 운전자 {2}",
+						myCar.date, myCar.model, myCar.rider
+				),
+				" "
+		);
 		while (st.hasMoreTokens())
 			System.out.println(st.nextToken());
 	}
