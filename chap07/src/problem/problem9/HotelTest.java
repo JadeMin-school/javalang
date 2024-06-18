b1799e43773227341fded25ade4bfccab69f938c
JadeMin <36400787+JadeMin@users.noreply.github.com>
2024-06-18 13:56:04 +0900

    upload: upload problem 8

---
+++ chap07/src/problem/problem9/HotelTest.java
@@ -0,0 +1,45 @@
+package problem.problem9;
+
+
+import java.util.ArrayList;
+
+class Hotel {
+	private ArrayList<Room> rooms = new ArrayList<Room>();
+
+	static class Room {
+		int num;
+		String name;
+
+		public Room(int num, String name) {
+			this.num = num;
+			this.name = name;
+		}
+
+		public void show() {
+			System.out.println(num + "번 방을 " + name + "가 예약했습니다.");
+		}
+	}
+
+	public Hotel() {
+
+	}
+
+	public void add(int num, String name) {
+		rooms.add(new Room(num, name));
+	}
+
+	public void show() {
+		for (Room room : rooms) {
+			room.show();
+		}
+	}
+}
+
+public class HotelTest {
+	public static void main(String[] args) {
+		Hotel hotel = new Hotel();
+		hotel.add(5, "호돌이");
+		hotel.add(7, "길동이");
+		hotel.show();
+	}
+}
